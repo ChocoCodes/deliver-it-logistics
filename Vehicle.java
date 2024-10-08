65eec4ad67bb53ad4099b2c8b3db46dbd97531a8
gen1chiro <juljavellana1218@gmail.com>
2024-10-08 17:49:37 +0800

    Added toCSVFormat

--- Vehicle.java
+++ Vehicle.java
@@ -60,7 +60,16 @@ public class Vehicle {
         );
     }
 
-    
+    public String[] toCSVFormat() {
+        return new String[]{
+                String.valueOf(vehicleID),
+                type,
+                licensePlate,
+                driver,
+                String.valueOf(capacityKG),
+                String.valueOf(isAvailable)
+        };
+    }
 
     public double getAvailableCapacity() {
         return capacityKG - currentCapacityKG;
